2cfaea7be45c347a8d4726eda01bb59d7ec03369
LakeBlue <LakeBlue184@qq.com>
2023-01-02 18:56:47 +0800

    1.编写用户界面 2.新增界面用图标

--- LeXun/pages/UserHome/UserHome.vue
+++ LeXun/pages/UserHome/UserHome.vue
@@ -1,19 +1,51 @@
 <template>
 	<view>
 		
+		<view class="top-bar">
+			
+			<view class="top-bar-left" @tap="BackWeb">
+					
+				<image src="../../static/images/common/back.png" class="back"></image>
+						
+			</view>
+			
+			<view class="top-bar-right">
+				
+				<view class="button" @tap="BackWeb">取
+				
+				消</view>
+					
+			</view>
+			
+		</view>	
+		
 	</view>
 </template>
 
-<script>
+<script>	
 	export default {
 		data() {
 			return {
 				
 			};
-		}
+		},
+		methods:{
+			
+			//返回登陆界面
+			BackWeb: function(){
+				
+				uni.navigateBack({
+					delta: 1
+				});
+				
+			}
+			
+		},
 	}
 </script>
 
 <style lang="scss">
+
+	@import"../../commons/css/MyCSS.scss";
 
 </style>
