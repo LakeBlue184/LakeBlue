b6dc792c7723129a6ff8b869f2be5893c7b983e8
LakeBlue <LakeBlue184@qq.com>
2023-03-18 00:35:56 +0800

    完成四个动画：
    1.好友申请框弹出
    
    2.申请界面底部按钮弹出
    
    3.头像随界面弹出缩放
    
    4.性别图标随头像缩放透明化

--- LeXun/pages/UserHome/UserHome.vue
+++ LeXun/pages/UserHome/UserHome.vue
@@ -33,13 +33,13 @@
 			
 			<view class="UserHead">
 				
-				<view class="sex" :style="{background:SexBg}">
+				<view class="sex" :style="{background:SexBg}" :animation="animationData3">
 					
 					<image src="../../static/images/UserHome/female.png"></image>
 					
 				</view>
 				
-				<image src="../../static/images/img/head3.jpeg" mode="aspectFill" class="UserHead-img"></image>
+				<image src="../../static/images/img/head3.jpeg" mode="aspectFill" class="UserHead-img" :animation="animationData2"></image>
 				
 			</view>
 			
@@ -55,11 +55,11 @@
 		
 		<view class="Bottom-bar">
 			
-			<view class="Bottom-btn">加为好友</view>
+			<view class="Bottom-btn" @tap="AddFriendAnimat">加为好友</view>
 			
 		</view>
 		
-		<view class="Add-Misg">
+		<view class="Add-Misg" :style="{height:addHeight+'px',bottom:-+addHeight+'px'}" :animation="animationData">
 			
 			<view class="name">{{user.name}}</view>
 			
@@ -67,9 +67,9 @@
 			
 		</view>
 		
-		<view class="Add-btn">
+		<view class="Add-btn" :animation="animationData1">
 			
-			<view class="close">取消</view>
+			<view class="cancel" @tap="AddFriendAnimat">取消</view>
 			<view class="send">发送</view>
 			
 		</view>
@@ -82,9 +82,21 @@
 		data() {
 			return {
 				
+				addHeight:'',
+				
+				animationData:{},
+				
+				animationData1:{},
+				
+				animationData2:{},
+				
+				animationData3:{},
+				
+				isAdd:false,
+								
 				SexBg:'rgba(215,245,241,1)',
 				
-				MyName:'一截春风炒香菇',
+				MyName:'张Star',
 				
 				user:{
 					
@@ -95,6 +107,13 @@
 				
 			};
 		},
+		
+		onReady: function(){
+		
+			this.getElementStyle();
+			
+		},
+		
 		methods:{
 			
 			//返回登陆界面
@@ -104,9 +123,76 @@
 					delta: 1
 				});
 				
+			},
+			
+			getElementStyle: function(){
+				
+				const query = uni.createSelectorQuery().in(this);
+				query.select('.bg').boundingClientRect(data => {
+				  console.log("得到布局位置信息" + JSON.stringify(data));
+				  console.log("节点离页面顶部的距离为" + data.top);
+				  this.addHeight = data.height-186;
+				}).exec();
+				
+			},
+			//添加好友动画
+			AddFriendAnimat:function(){
+				
+				this.isAdd = !this.isAdd;
+				
+				var animation = uni.createAnimation({//好友申请框
+					
+					duration: 750,
+					timingFunction: 'ease',
+					
+				})
+				
+				var animation1 = uni.createAnimation({//界面底部按钮
+					
+					duration: 750,
+					timingFunction: 'ease',
+					
+				})
+				
+				var animation2 = uni.createAnimation({//头像缩放
+					
+					duration: 750,
+					timingFunction: 'ease',
+					
+				})
+				
+				var animation3 = uni.createAnimation({//
+					
+					duration: 750,
+					timingFunction: 'ease',
+					
+				})
+				
+				if(this.isAdd){
+					
+					animation.bottom(0).step()
+					animation1.bottom(0).step()
+					animation2.width(120).height(120).top(40).step()
+					animation3.opacity(0).step()
+					
+				}else{
+					
+					animation.bottom(-this.addHeight).step()
+					animation1.bottom(-100).step()
+					animation2.width(200).height(200).top(0).step()
+					animation3.opacity(1).step()
+					
+				}
+
+				this.animationData = animation.export()
+				this.animationData1 = animation1.export()
+				this.animationData2 = animation2.export()
+				this.animationData3 = animation3.export()
+				
 			}
 			
 		},
+				
 	}
 </script>
 
@@ -155,6 +241,8 @@
 		.UserHead{
 			
 			position: relative;
+			left:0rpx;
+			bottom: 105rpx;
 			margin: 0 auto;
 			width: 412rpx;
 			height: 412rpx;
@@ -162,7 +250,7 @@
 			.sex{
 				
 				position: absolute;
-				bottom: 22rpx;
+				bottom: 20rpx;
 				right: 22rpx;
 				z-index: 11;
 				width: 64rpx;
@@ -181,10 +269,12 @@
 			.UserHead-img{
 				
 				z-index: 10;
+				top:0;
 				width: 400rpx;
 				height: 400rpx;
 				border-radius: 48rpx;
 				border: 6rpx solid rgba(225, 225, 225, 1);
+				box-shadow: 0rpx 36rpx 40rpx 0rpx rgba(39,40,50,0.1);
 				
 			}
 			
@@ -252,11 +342,11 @@
 	.Add-Misg{
 		
 		position: fixed;
-		bottom: 0;
+		//bottom: 0;
 		width: 100%;
 		box-sizing: border-box;
 		padding: 0 56rpx;
-		height: 1252rpx;
+		//height: 1252rpx;
 		background: #FFFF;
 		border-radius: 40px 40px 0px 0px;
 		
@@ -272,7 +362,8 @@
 		.Add-Main{
 			
 			padding:18rpx 22rpx;
-			//width: 100%;
+			box-sizing: border-box;
+			width: 100%;
 			height: 420rpx;
 			background: #F3F4F6;
 			border-radius: $uni-border-radius-base;
@@ -287,7 +378,7 @@
 	.Add-btn{
 		
 		position: fixed;
-		bottom: 0rpx;
+		bottom: -104rpx;
 		width: 100%;
 		height: 104rpx;
 		box-sizing: border-box;
@@ -295,7 +386,7 @@
 		display: flex;
 		z-index: 100;
 		
-		.close{
+		.cancel{
 			
 			text-align: center;
 			line-height: 80rpx;
